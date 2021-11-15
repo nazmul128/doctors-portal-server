d0bdbe27610c46f34a95f0d1c573f65705c863db
nazmul128 <shafayat536@gmail.com>
2021-11-16 01:19:53 +0600

    code added

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const { MongoClient } = require('mongodb');
 const ObjectId = require('mongodb').ObjectId;
 const admin = require("firebase-admin");
 const stripe=require('stripe')(process.env.STRIPE_SECRET);
+const fileUpload=require('express-fileupload');
 
 const app = express()
 const port = process.env.PORT||5000;
@@ -18,6 +19,7 @@ admin.initializeApp({
 //middleware
 app.use(cors());
 app.use(express.json());
+app.use(fileUpload());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.s3ngn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -45,6 +47,7 @@ async function run(){
        const database=client.db('doctors_portal')
        const appointmentCollection=database.collection('appointments');
        const usersCollection=database.collection('users');
+       const doctorsCollection=database.collection('doctors');
       
        //GET
        app.get('/appointments',verifyToken, async(req,res)=>{
@@ -85,6 +88,30 @@ async function run(){
            }
          };
          const result=await appointmentCollection.updateOne(filter,updateDoc);
+       })
+
+
+        //get all doctors
+       app.get('/doctors', async(req,res)=>{
+        const cursor=doctorsCollection.find({});
+        const doctors=await cursor.toArray();
+        res.json(doctors);
+       })
+
+       app.post('/doctors', async(req,res)=>{
+         const name=req.body.name;
+         const email=req.body.email;
+         const pic=req.files.image;
+         const picData=pic.data;
+         const encodedPic= picData.toString('base64');
+         const imageBuffer= Buffer.from(encodedPic, 'base64');
+        const doctor={
+          name,
+          email,
+          image: imageBuffer
+        }
+         const result=await doctorsCollection.insertOne(doctor); 
+         res.json(result)
        })
 
        app.get('/users/:email',async(req,res)=>{
